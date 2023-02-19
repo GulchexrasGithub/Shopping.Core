7caddc9d9e5089b819eba9b1da4df9b4d4e64cea
GulchexrasGithub <burgulchexra@gmail.com>
2023-02-19 13:55:29 +0500

    DATA: User added

---
+++ Shopping/Shopping.Core/Models/Users/Role.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Gulchekhra Burkhonova
+// Free To Use To Manage The Shopping In Markets 
+// ---------------------------------------------------------------
+
+namespace Shopping.Core.Models.Users
+{
+    public enum Role
+    {
+        Admin,
+        User
+    }
+}
