5d5fe718bd0d365005a1c51549e5138ebaa1b11c
GulchexrasGithub <burgulchexra@gmail.com>
2023-02-19 15:38:55 +0500

    DATA & MIGRATION: Product Add

---
+++ Shopping/Shopping.Core/Brokers/Storages/StorageBroker.Product.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Core.Models.Products;
+
+namespace Shopping.Core.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
