94f288ea6ee092e3d913fdef9c1c20a0b6607fab
GulchexrasGithub <burgulchexra@gmail.com>
2023-02-19 19:21:20 +0500

    BROKERS: Product CRUD

---
+++ Shopping/Shopping.Core/Brokers/Storages/IStorageBroker.Product.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Gulchekhra Burkhonova
+// Free To Use To Manage The Shopping In Markets 
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Shopping.Core.Models.Products;
+
+namespace Shopping.Core.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Product> InsertProductAsync(Product product);
+        IQueryable<Product> SelectAllProducts();
+        ValueTask<Product> SelectProductByIdAsync(Guid productId);
+        ValueTask<Product> UpdateProductAsync(Product product);
+        ValueTask<Product> DeleteProductAsync(Product product);
+    }
+}
--- Shopping/Shopping.Core/Brokers/Storages/StorageBroker.User.cs
+++ Shopping/Shopping.Core/Brokers/Storages/StorageBroker.User.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Shopping.Core.Models.Users;
 
 namespace Shopping.Core.Brokers.Storages
@@ -9,20 +6,5 @@ namespace Shopping.Core.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<User> Users { get; set; }
-
-        public async ValueTask<User> InsertUserAsync(User user) =>
-            await InsertAsync(user);
-
-        public IQueryable<User> SelectAllUsers() =>
-            SelectAll<User>();
-
-        public async ValueTask<User> SelectUserByIdAsync(Guid id) =>
-            await SelectAsync<User>(id);
-
-        public async ValueTask<User> UpdateUserAsync(User user) =>
-           await UpdateAsync(user);
-
-        public async ValueTask<User> DeleteUserAsync(User user) =>
-            await DeleteAsync(user);
     }
 }
